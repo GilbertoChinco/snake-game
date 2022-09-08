3259901cfded511bdf1e164bf23a5c2b2f37bb20
Gilberto Chincoya <chinco.gilberto@gmail.com>
2022-09-08 17:58:16 -0500

    Creake food class

---
+++ food.py
@@ -0,0 +1,25 @@
+from turtle import Turtle
+import random
+#
+class Food(Turtle):
+    #
+    def __init__(self):
+        super().__init__()
+        self.penup()
+        self.hideturtle()
+        self.shape("circle")
+        self.color("red")
+        self.radius = 10
+        self.set_random_position()   
+    #
+    def genrate_random_position(self, width, height):
+        offset = 20
+        x_random_position = random.randint(- width / 2 + offset, width / 2 - offset)
+        y_random_position = random.randint(- height / 2 + offset, height / 2 - offset)
+        return x_random_position, y_random_position
+    #
+    def set_random_position(self):
+        x_food_pos, y_food_pos = self.genrate_random_position(500, 500)
+        self.setposition(x_food_pos, y_food_pos)
+        self.showturtle()
+    #
