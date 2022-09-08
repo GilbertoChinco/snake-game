b616481741de498eafd88d5a5a0e3ca3688f5fca
Gilberto Chincoya <chinco.gilberto@gmail.com>
2022-09-08 18:38:55 -0500

    update the food and snake class

--- food.py
+++ food.py
@@ -3,14 +3,14 @@ import random
 #
 class Food(Turtle):
     #
-    def __init__(self):
+    def __init__(self, width, height):
         super().__init__()
         self.penup()
         self.hideturtle()
         self.shape("circle")
         self.color("red")
-        self.radius = 10
-        self.set_random_position()   
+        self.radius = 12
+        self.set_random_position(width, height)   
     #
     def genrate_random_position(self, width, height):
         offset = 20
@@ -18,8 +18,7 @@ class Food(Turtle):
         y_random_position = random.randint(- height / 2 + offset, height / 2 - offset)
         return x_random_position, y_random_position
     #
-    def set_random_position(self):
-        x_food_pos, y_food_pos = self.genrate_random_position(500, 500)
+    def set_random_position(self, width, height):
+        x_food_pos, y_food_pos = self.genrate_random_position(width, height)
         self.setposition(x_food_pos, y_food_pos)
         self.showturtle()
-    #
--- snake.py
+++ snake.py
@@ -1,12 +1,13 @@
 from turtle import Turtle
 #
 class Snake(Turtle):
-
+    #
     def __init__(self):
         super().__init__()
         self.starting_position = [(0, 0), (-20, 0), (-40, 0)]
         self.segments = []
         self.create_body()
+        self.head_segment = self.segments[0]
     #
     def create_body(self):
         for position in self.starting_position:
@@ -16,7 +17,7 @@ class Snake(Turtle):
         new_segment = Turtle("square")
         new_segment.color("white")
         new_segment.penup()
-        new_segment.goto(position)
+        new_segment.setposition(position)
         self.segments.append(new_segment)
     #
     def extend_body(self):
@@ -27,39 +28,37 @@ class Snake(Turtle):
             new_x = self.segments[i - 1].xcor()
             new_y = self.segments[i - 1].ycor()
             self.segments[i].setposition(new_x, new_y)
-        self.segments[0].forward(20)
+        self.head_segment.forward(20)
     #
     def direction_north(self):
-        if self.segments[0].heading() != 270:
-            self.segments[0].setheading(90)
+        if self.head_segment.heading() != 270:
+            self.head_segment.setheading(90)
     #
     def direction_south(self):
-        if self.segments[0].heading() != 90:
-            self.segments[0].setheading(270)
+        if self.head_segment.heading() != 90:
+            self.head_segment.setheading(270)
     #
     def direction_west(self):
-        if self.segments[0].heading() != 0:
-            self.segments[0].setheading(180)
+        if self.head_segment.heading() != 0:
+            self.head_segment.setheading(180)
     #
     def direction_east(self):
-        if self.segments[0].heading() != 180:
-            self.segments[0].setheading(0)
+        if self.head_segment.heading() != 180:
+            self.head_segment.setheading(0)
     #
     def is_position_out_of_bounds(self):
-        x_position = self.segments[0].xcor()
-        y_position = self.segments[0].ycor()
+        x_position = self.head_segment.xcor()
+        y_position = self.head_segment.ycor()
         if abs(x_position) > 250 or abs(y_position) > 250:
             return True
+        #
         return False
     #
     def is_collioned_with_food(self, food):
-        snake_x_position = self.segments[0].xcor()
-        snake_y_position = self.segments[0].ycor()
+        snake_x_position = self.head_segment.xcor()
+        snake_y_position = self.head_segment.ycor()
         food_x_position = food.xcor()
         food_y_position = food.ycor()
-        # (x - h) ** 2 + (y - k) ** 2 <= r ** 2
-        # (x, y): snake position
-        # (h, k): food position
         x_part = (snake_x_position - food_x_position) ** 2
         y_part = (snake_y_position - food_y_position) ** 2
         radius = food.radius ** 2
@@ -67,17 +66,18 @@ class Snake(Turtle):
             self.extend_body()
             food.hideturtle()
             return True
+        #
         return False
     #
     def is_collioned_with_tail(self):
-        head = self.segments[0]
+        head = self.head_segment
         x_cor_head = head.xcor()
         y_cor_head = head.ycor()
         for i in range(1, len(self.segments)):
             x_cor_segment = self.segments[i].xcor()
             y_cor_segment = self.segments[i].ycor()
-
+            #
             if x_cor_head == x_cor_segment and y_cor_head == y_cor_segment:
                 return True
-
+        #
         return False
